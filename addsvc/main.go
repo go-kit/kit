c937cf4caa06e2599f0c418ba1b04b853ff5e9e6
Peter Bourgon <peter@bourgon.org>
2015-03-14 11:39:09 +0100

    addsvc: add Thrift bindings

--- addsvc/main.go
+++ addsvc/main.go
@@ -12,10 +12,13 @@ import (
 	"syscall"
 	"time"
 
+	"git.apache.org/thrift.git/lib/go/thrift"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"github.com/peterbourgon/gokit/addsvc/pb"
+	thriftadd "github.com/peterbourgon/gokit/addsvc/thrift/gen-go/add"
 	"github.com/peterbourgon/gokit/metrics"
 	"github.com/peterbourgon/gokit/metrics/expvar"
 	"github.com/peterbourgon/gokit/metrics/statsd"
@@ -24,8 +27,12 @@ import (
 
 func main() {
 	var (
-		httpJSONAddr = flag.String("http.json.addr", ":8001", "Address for HTTP/JSON server")
-		grpcTCPAddr  = flag.String("grpc.tcp.addr", ":8002", "Address for gRPC (TCP) server")
+		httpJSONAddr     = flag.String("http.json.addr", ":8001", "Address for HTTP/JSON server")
+		grpcTCPAddr      = flag.String("grpc.tcp.addr", ":8002", "Address for gRPC (TCP) server")
+		thriftTCPAddr    = flag.String("thrift.tcp.addr", ":8003", "Address for Thrift (TCP) server")
+		thriftProtocol   = flag.String("thrift.protocol", "binary", "binary, compact, json, simplejson")
+		thriftBufferSize = flag.Int("thrift.transport.bufsz", 0, "Thrift transport buffer size (0 = unbuffered)")
+		thriftFramed     = flag.Bool("thrift.framed", false, "use framed transport")
 	)
 	flag.Parse()
 
@@ -91,6 +98,57 @@ func main() {
 		mux.Handle("/add", handler)
 		log.Printf("HTTP/JSON server on %s", *httpJSONAddr)
 		errc <- http.ListenAndServe(*httpJSONAddr, mux)
+	}()
+
+	// Transport: Thrift
+	go func() {
+		ctx, cancel := context.WithCancel(root)
+		defer cancel()
+
+		var protocolFactory thrift.TProtocolFactory
+		switch *thriftProtocol {
+		case "binary":
+			protocolFactory = thrift.NewTBinaryProtocolFactoryDefault()
+		case "compact":
+			protocolFactory = thrift.NewTCompactProtocolFactory()
+		case "json":
+			protocolFactory = thrift.NewTJSONProtocolFactory()
+		case "simplejson":
+			protocolFactory = thrift.NewTSimpleJSONProtocolFactory()
+		default:
+			errc <- fmt.Errorf("invalid Thrift protocol %q", *thriftProtocol)
+			return
+		}
+
+		var transportFactory thrift.TTransportFactory
+		if *thriftBufferSize > 0 {
+			transportFactory = thrift.NewTBufferedTransportFactory(*thriftBufferSize)
+		} else {
+			transportFactory = thrift.NewTTransportFactory()
+		}
+
+		if *thriftFramed {
+			transportFactory = thrift.NewTFramedTransportFactory(transportFactory)
+		}
+
+		transport, err := thrift.NewTServerSocket(*thriftTCPAddr)
+		if err != nil {
+			errc <- err
+			return
+		}
+
+		var handler thriftadd.AddService
+		handler = thriftBinding{ctx, e}
+		field := metrics.Field{Key: "transport", Value: "thrift"}
+		handler = thriftInstrument(requests.With(field), duration.With(field), handler)
+
+		log.Printf("Thrift (TCP) server on %s", *thriftTCPAddr)
+		errc <- thrift.NewTSimpleServer4(
+			thriftadd.NewAddServiceProcessor(thriftBinding{ctx, e}),
+			transport,
+			transportFactory,
+			protocolFactory,
+		).Serve()
 	}()
 
 	log.Fatal(<-errc)
---
+++ addsvc/thrift/compile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# https://thrift.apache.org/tutorial/go
+thrift -r --gen go add.thrift
