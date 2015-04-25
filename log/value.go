014f41460d7b0401c84122ce715ce643d7c97d98
Chris Hines <github@cs-guy.com>
2015-04-25 00:25:57 -0400

    Update comments to match recent changes and improve layout.

--- log/value.go
+++ log/value.go
@@ -10,9 +10,9 @@ import (
 // dynamic value which is re-evaluated with each log event.
 type Valuer func() interface{}
 
-// BindValues returns a slice with all value elements (odd indexes) that
-// implement Valuer replaced with the result of calling their Value method. If
-// no value elements implement Valuer, the original slice is returned.
+// BindValues returns a slice with all value elements (odd indexes) containing
+// a Valuer replaced by their generated value. If no Valuers are found, the
+// original slice is returned.
 func BindValues(keyvals ...interface{}) []interface{} {
 	if !containsValuer(keyvals) {
 		return keyvals
@@ -39,26 +39,32 @@ func containsValuer(keyvals []interface{}) bool {
 }
 
 var (
-	// Timestamp returns a Valuer that invokes the underlying function when bound,
-	// returning a time.Time. Users will probably want to use DefaultTimestamp or
-	// DefaultTimestampUTC.
+	// Timestamp returns a Valuer that invokes the underlying function when
+	// bound, returning a time.Time. Users will probably want to use
+	// DefaultTimestamp or DefaultTimestampUTC.
 	Timestamp = func(t func() time.Time) Valuer {
 		return func() interface{} { return t() }
 	}
-	// DefaultTimestamp is a Timestamp Valuer that returns the current wallclock
-	// time, respecting time zones, when bound.
+
+	// DefaultTimestamp is a Timestamp Valuer that returns the current
+	// wallclock time, respecting time zones, when bound.
 	DefaultTimestamp = Timestamp(time.Now)
+
 	// DefaultTimestampUTC wraps DefaultTimestamp but ensures the returned
 	// time is always in UTC. Note that it invokes DefaultTimestamp, and so
 	// reflects any changes to the DefaultTimestamp package global.
 	DefaultTimestampUTC = Timestamp(func() time.Time {
 		return DefaultTimestamp().(time.Time).UTC()
 	})
-	// Caller is a Valuer that returns a file and line from a specified depth in
-	// the callstack. Users will probably want to use DefaultCaller.
+)
+
+var (
+	// Caller is a Valuer that returns a file and line from a specified depth
+	// in the callstack. Users will probably want to use DefaultCaller.
 	Caller = func(depth int) Valuer {
 		return func() interface{} { return stack.Caller(depth) }
 	}
+
 	// DefaultCaller is a Valuer that returns the file and line where the Log
 	// method was invoked.
 	DefaultCaller = Caller(3)
