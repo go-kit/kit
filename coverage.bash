31d842cb76a50e8c2274ba02097e5fdbb962d3f5
Peter Bourgon <peter@bourgon.org>
2015-05-24 00:48:47 +0200

    Add coverage.bash script; make scripts consistent

---
+++ coverage.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script runs the cover tool on all packages with test files. If you set
+# an "HTML" environment variable, it will additionally open the web-based
+# coverage visualizer for each package.
+
+function go_files { find . -name '*_test.go' ; }
+function filter { grep -v '/_' ; }
+function remove_relative_prefix { sed -e 's/^\.\///g' ; }
+
+function directories {
+	go_files | filter | remove_relative_prefix | while read f
+	do
+		dirname $f
+	done
+}
+
+function unique_directories { directories | sort | uniq ; }
+
+function package_names {
+	unique_directories | while read d
+	do
+		echo github.com/go-kit/kit/$d
+	done
+}
+
+function report {
+	package_names | while read pkg
+	do
+		go test -coverprofile=cover.out $pkg
+		if [ -n "${HTML+x}" ]
+		then
+			go tool cover -html=cover.out
+		fi
+	done
+	rm cover.out
+}
+
+report
+
