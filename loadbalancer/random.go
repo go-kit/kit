7205a7a70f9331ac81f9e3d5478ad7f27d9bc88e
Peter Bourgon <peter@bourgon.org>
2015-06-20 19:08:12 -0700

    Take 2 on the load balancer

---
+++ loadbalancer/random.go
@@ -0,0 +1,22 @@
+package loadbalancer
+
+import (
+	"math/rand"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
+// Random returns a load balancer that yields random endpoints.
+func Random(p Publisher) LoadBalancer {
+	return random{newCache(p)}
+}
+
+type random struct{ *cache }
+
+func (r random) Get() (endpoint.Endpoint, error) {
+	endpoints := r.cache.get()
+	if len(endpoints) <= 0 {
+		return nil, ErrNoEndpointsAvailable
+	}
+	return endpoints[rand.Intn(len(endpoints))], nil
+}
