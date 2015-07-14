bd48c1597d0292c74e5c1815dc0c8b19863cda7b
Peter Bourgon <peter@bourgon.org>
2015-07-14 09:45:38 +0200

    log/levels: clean up some comments and var names

--- log/levels/levels.go
+++ log/levels/levels.go
@@ -2,15 +2,18 @@ package levels
 
 import "github.com/go-kit/kit/log"
 
-// Levels provides leveled logging.
+// Levels provides a leveled logging wrapper around a logger. It has five
+// levels: debug, info, warning (warn), error, and critical (crit). If you
+// want a different set of levels, you can create your own levels type very
+// easily, and you can elide the configuration.
 type Levels struct {
-	ctx  log.Context
-	opts *config
+	ctx log.Context
+	cfg *config
 }
 
-// New creates a new leveled logger.
+// New creates a new leveled logger, wrapping the passed logger.
 func New(logger log.Logger, options ...Option) Levels {
-	opts := &config{
+	cfg := &config{
 		levelKey:   "level",
 		debugValue: "debug",
 		infoValue:  "info",
@@ -19,45 +22,45 @@ func New(logger log.Logger, options ...Option) Levels {
 		critValue:  "crit",
 	}
 	for _, option := range options {
-		option(opts)
+		option(cfg)
 	}
 	return Levels{
-		ctx:  log.NewContext(logger),
-		opts: opts,
+		ctx: log.NewContext(logger),
+		cfg: cfg,
 	}
 }
 
 // With returns a new leveled logger that includes keyvals in all log events.
 func (l Levels) With(keyvals ...interface{}) Levels {
 	return Levels{
-		ctx:  l.ctx.With(keyvals...),
-		opts: l.opts,
+		ctx: l.ctx.With(keyvals...),
+		cfg: l.cfg,
 	}
 }
 
 // Debug logs a debug event along with keyvals.
 func (l Levels) Debug(keyvals ...interface{}) error {
-	return l.ctx.WithPrefix(l.opts.levelKey, l.opts.debugValue).Log(keyvals...)
+	return l.ctx.WithPrefix(l.cfg.levelKey, l.cfg.debugValue).Log(keyvals...)
 }
 
 // Info logs an info event along with keyvals.
 func (l Levels) Info(keyvals ...interface{}) error {
-	return l.ctx.WithPrefix(l.opts.levelKey, l.opts.infoValue).Log(keyvals...)
+	return l.ctx.WithPrefix(l.cfg.levelKey, l.cfg.infoValue).Log(keyvals...)
 }
 
 // Warn logs a warn event along with keyvals.
 func (l Levels) Warn(keyvals ...interface{}) error {
-	return l.ctx.WithPrefix(l.opts.levelKey, l.opts.warnValue).Log(keyvals...)
+	return l.ctx.WithPrefix(l.cfg.levelKey, l.cfg.warnValue).Log(keyvals...)
 }
 
 // Error logs an error event along with keyvals.
 func (l Levels) Error(keyvals ...interface{}) error {
-	return l.ctx.WithPrefix(l.opts.levelKey, l.opts.errorValue).Log(keyvals...)
+	return l.ctx.WithPrefix(l.cfg.levelKey, l.cfg.errorValue).Log(keyvals...)
 }
 
 // Crit logs a crit event along with keyvals.
 func (l Levels) Crit(keyvals ...interface{}) error {
-	return l.ctx.WithPrefix(l.opts.levelKey, l.opts.critValue).Log(keyvals...)
+	return l.ctx.WithPrefix(l.cfg.levelKey, l.cfg.critValue).Log(keyvals...)
 }
 
 type config struct {
@@ -76,35 +79,35 @@ type Option func(*config)
 // Key sets the key for the field used to indicate log level. By default,
 // the key is "level".
 func Key(key string) Option {
-	return func(o *config) { o.levelKey = key }
+	return func(c *config) { c.levelKey = key }
 }
 
 // DebugValue sets the value for the field used to indicate the debug log
 // level. By default, the value is "debug".
 func DebugValue(value string) Option {
-	return func(o *config) { o.debugValue = value }
+	return func(c *config) { c.debugValue = value }
 }
 
-// InfoValue sets the value for the field used to indicate the debug log
-// level. By default, the value is "info".
+// InfoValue sets the value for the field used to indicate the info log level.
+// By default, the value is "info".
 func InfoValue(value string) Option {
-	return func(o *config) { o.infoValue = value }
+	return func(c *config) { c.infoValue = value }
 }
 
-// WarnValue sets the value for the field used to indicate the debug log
-// level. By default, the value is "debug".
+// WarnValue sets the value for the field used to indicate the warning log
+// level. By default, the value is "warn".
 func WarnValue(value string) Option {
-	return func(o *config) { o.warnValue = value }
+	return func(c *config) { c.warnValue = value }
 }
 
-// ErrorValue sets the value for the field used to indicate the debug log
+// ErrorValue sets the value for the field used to indicate the error log
 // level. By default, the value is "error".
 func ErrorValue(value string) Option {
-	return func(o *config) { o.errorValue = value }
+	return func(c *config) { c.errorValue = value }
 }
 
-// CritValue sets the value for the field used to indicate the debug log
-// level. By default, the value is "debug".
+// CritValue sets the value for the field used to indicate the critical log
+// level. By default, the value is "crit".
 func CritValue(value string) Option {
-	return func(o *config) { o.critValue = value }
+	return func(c *config) { c.critValue = value }
 }
