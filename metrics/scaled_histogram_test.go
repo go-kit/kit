822f940bfea8bdebd64dcf250aaaf5d0891b1a9c
Peter Bourgon <peter@bourgon.org>
2015-07-22 00:36:03 +0100

    metrics: improve test coverage

--- metrics/scaled_histogram_test.go
+++ metrics/scaled_histogram_test.go
@@ -8,13 +8,16 @@ import (
 )
 
 func TestScaledHistogram(t *testing.T) {
-	quantiles := []int{50, 90, 99}
-	scale := int64(10)
-	metricName := "test_scaled_histogram"
+	var (
+		quantiles  = []int{50, 90, 99}
+		scale      = int64(10)
+		metricName = "test_scaled_histogram"
+	)
 
 	var h metrics.Histogram
 	h = expvar.NewHistogram(metricName, 0, 1000, 3, quantiles...)
 	h = metrics.NewScaledHistogram(h, scale)
+	h = h.With(metrics.Field{Key: "a", Value: "b"})
 
 	const seed, mean, stdev = 333, 500, 100          // input values
 	populateNormalHistogram(t, h, seed, mean, stdev) // will be scaled down
