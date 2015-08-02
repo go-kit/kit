576ad0bd2e3f70c2382e81e6491fada7a6d65034
Peter Bourgon <peter@bourgon.org>
2015-08-02 22:12:22 +0200

    Add README and Retry component

--- loadbalancer/static/publisher.go
+++ loadbalancer/static/publisher.go
@@ -1,9 +1,35 @@
 package static
 
-import "github.com/go-kit/kit/endpoint"
+import (
+	"sync"
+
+	"github.com/go-kit/kit/endpoint"
+)
 
 // Publisher yields the same set of static endpoints.
-type Publisher []endpoint.Endpoint
+type Publisher struct {
+	mtx       sync.RWMutex
+	endpoints []endpoint.Endpoint
+}
+
+// NewPublisher returns a static endpoint Publisher.
+func NewPublisher(endpoints []endpoint.Endpoint) *Publisher {
+	return &Publisher{
+		endpoints: endpoints,
+	}
+}
 
 // Endpoints implements the Publisher interface.
-func (p Publisher) Endpoints() ([]endpoint.Endpoint, error) { return p, nil }
+func (p *Publisher) Endpoints() ([]endpoint.Endpoint, error) {
+	p.mtx.RLock()
+	defer p.mtx.RUnlock()
+	return p.endpoints, nil
+}
+
+// Replace is a utility method to swap out the underlying endpoints of an
+// existing static publisher. It's useful mostly for testing.
+func (p *Publisher) Replace(endpoints []endpoint.Endpoint) {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+	p.endpoints = endpoints
+}
