a875889c907666b52b31e8c6d5867d9d8fbb1e57
Chris Hines <github@cs-guy.com>
2015-08-06 22:48:56 -0400

    Recover from panics caused by dereferencing a nil pointer to a value receiver implementation of the fmt.Stringer or error interfaces.

--- log/json_logger_test.go
+++ log/json_logger_test.go
@@ -33,6 +33,44 @@ func TestJSONLoggerMissingValue(t *testing.T) {
 	}
 }
 
+func TestJSONLoggerNilStringerKey(t *testing.T) {
+	t.Parallel()
+
+	buf := &bytes.Buffer{}
+	logger := log.NewJSONLogger(buf)
+	if err := logger.Log((*stringer)(nil), "v"); err != nil {
+		t.Fatal(err)
+	}
+	if want, have := `{"NULL":"v"}`+"\n", buf.String(); want != have {
+		t.Errorf("want %#v, have %#v", want, have)
+	}
+}
+
+func TestJSONLoggerNilErrorValue(t *testing.T) {
+	t.Parallel()
+
+	buf := &bytes.Buffer{}
+	logger := log.NewJSONLogger(buf)
+	if err := logger.Log("err", (*stringError)(nil)); err != nil {
+		t.Fatal(err)
+	}
+	if want, have := `{"err":null}`+"\n", buf.String(); want != have {
+		t.Errorf("want %#v, have %#v", want, have)
+	}
+}
+
+type stringer string
+
+func (s stringer) String() string {
+	return string(s)
+}
+
+type stringError string
+
+func (s stringError) Error() string {
+	return string(s)
+}
+
 func BenchmarkJSONLoggerSimple(b *testing.B) {
 	benchmarkRunner(b, log.NewJSONLogger(ioutil.Discard), baseMessage)
 }
