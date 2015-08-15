32eda3f30e6fd67ddf64cdcf3b23e1083db1094e
Peter Bourgon <peter@bourgon.org>
2015-08-15 18:04:03 +0200

    circuitbreaker: improve test reporting

--- circuitbreaker/util_test.go
+++ circuitbreaker/util_test.go
@@ -2,6 +2,9 @@ package circuitbreaker_test
 
 import (
 	"errors"
+	"fmt"
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	"golang.org/x/net/context"
@@ -10,6 +13,9 @@ import (
 )
 
 func testFailingEndpoint(t *testing.T, breaker endpoint.Middleware, primeWith int, shouldPass func(int) bool, openCircuitError string) {
+	_, file, line, _ := runtime.Caller(1)
+	caller := fmt.Sprintf("%s:%d", filepath.Base(file), line)
+
 	// Create a mock endpoint and wrap it with the breaker.
 	m := mock{}
 	var e endpoint.Endpoint
@@ -19,7 +25,7 @@ func testFailingEndpoint(t *testing.T, breaker endpoint.Middleware, primeWith in
 	// Prime the endpoint with successful requests.
 	for i := 0; i < primeWith; i++ {
 		if _, err := e(context.Background(), struct{}{}); err != nil {
-			t.Fatalf("during priming, got error: %v", err)
+			t.Fatalf("%s: during priming, got error: %v", caller, err)
 		}
 	}
 
@@ -30,7 +36,7 @@ func testFailingEndpoint(t *testing.T, breaker endpoint.Middleware, primeWith in
 	// The first several should be allowed through and yield our error.
 	for i := 0; shouldPass(i); i++ {
 		if _, err := e(context.Background(), struct{}{}); err != m.err {
-			t.Fatalf("want %v, have %v", m.err, err)
+			t.Fatalf("%s: want %v, have %v", caller, m.err, err)
 		}
 	}
 	thru := m.thru
@@ -38,13 +44,13 @@ func testFailingEndpoint(t *testing.T, breaker endpoint.Middleware, primeWith in
 	// But the rest should be blocked by an open circuit.
 	for i := 0; i < 10; i++ {
 		if _, err := e(context.Background(), struct{}{}); err.Error() != openCircuitError {
-			t.Fatalf("want %q, have %q", openCircuitError, err.Error())
+			t.Fatalf("%s: want %q, have %q", caller, openCircuitError, err.Error())
 		}
 	}
 
 	// Make sure none of those got through.
 	if want, have := thru, m.thru; want != have {
-		t.Errorf("want %d, have %d", want, have)
+		t.Errorf("%s: want %d, have %d", caller, want, have)
 	}
 }
 
