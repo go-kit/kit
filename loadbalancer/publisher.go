2766c933c9cdd1b1cc0243f48851a18a64b2e83b
Peter Bourgon <peter@bourgon.org>
2015-09-26 20:15:05 +0200

    loadbalancer: ErrPublisherStopped no longer used

--- loadbalancer/publisher.go
+++ loadbalancer/publisher.go
@@ -1,10 +1,6 @@
 package loadbalancer
 
-import (
-	"errors"
-
-	"github.com/go-kit/kit/endpoint"
-)
+import "github.com/go-kit/kit/endpoint"
 
 // Publisher describes something that provides a set of identical endpoints.
 // Different publisher implementations exist for different kinds of service
@@ -12,7 +8,3 @@ import (
 type Publisher interface {
 	Endpoints() ([]endpoint.Endpoint, error)
 }
-
-// ErrPublisherStopped is returned by publishers when the underlying
-// implementation has been terminated and can no longer serve requests.
-var ErrPublisherStopped = errors.New("publisher stopped")
