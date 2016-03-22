72334e116d13e56b1c618ddae8e19d62f7aef1d1
Peter Bourgon <peter@bourgon.org>
2016-03-22 09:10:10 +0100

    Minor cleanup to addsvc/client/grpc

--- examples/addsvc/client/grpc/encode_decode.go
+++ examples/addsvc/client/grpc/encode_decode.go
@@ -1,45 +1,38 @@
 package grpc
 
 import (
+	"golang.org/x/net/context"
+
 	"github.com/go-kit/kit/examples/addsvc/pb"
 	"github.com/go-kit/kit/examples/addsvc/server"
-	"golang.org/x/net/context"
 )
 
-func encodeSumRequest(ctx context.Context, req interface{}) (interface{}, error) {
-	sumRequest := req.(server.SumRequest)
-
-	pbRequest := &pb.SumRequest{
-		A: int64(sumRequest.A),
-		B: int64(sumRequest.B),
-	}
-	return pbRequest, nil
+func encodeSumRequest(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(server.SumRequest)
+	return &pb.SumRequest{
+		A: int64(req.A),
+		B: int64(req.B),
+	}, nil
 }
 
-func encodeConcatRequest(ctx context.Context, req interface{}) (interface{}, error) {
-	concatRequest := req.(server.ConcatRequest)
-
-	pbRequest := &pb.ConcatRequest{
-		A: concatRequest.A,
-		B: concatRequest.B,
-	}
-	return pbRequest, nil
+func encodeConcatRequest(ctx context.Context, request interface{}) (interface{}, error) {
+	req := request.(server.ConcatRequest)
+	return &pb.ConcatRequest{
+		A: req.A,
+		B: req.B,
+	}, nil
 }
 
-func decodeSumResponse(ctx context.Context, resp interface{}) (interface{}, error) {
-	pbResponse := resp.(pb.SumReply)
-
-	sumResponse := &server.SumResponse{
-		V: int(pbResponse.V),
-	}
-	return sumResponse, nil
+func decodeSumResponse(ctx context.Context, response interface{}) (interface{}, error) {
+	resp := response.(pb.SumReply)
+	return &server.SumResponse{
+		V: int(resp.V),
+	}, nil
 }
 
-func decodeConcatResponse(ctx context.Context, resp interface{}) (interface{}, error) {
-	pbResponse := resp.(pb.ConcatReply)
-
-	concatResponse := &server.ConcatResponse{
-		V: pbResponse.V,
-	}
-	return concatResponse, nil
+func decodeConcatResponse(ctx context.Context, response interface{}) (interface{}, error) {
+	resp := response.(pb.ConcatReply)
+	return &server.ConcatResponse{
+		V: resp.V,
+	}, nil
 }
