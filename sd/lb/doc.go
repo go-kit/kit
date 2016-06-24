f7f1de9e068e61acb72e39da84de500c33b2aaf6
Peter Bourgon <peter@bourgon.org>
2016-06-24 16:40:01 +0000

    sd/*: doc.go

--- sd/lb/doc.go
+++ sd/lb/doc.go
@@ -1,5 +1,4 @@
-// Package lb deals with client-side load balancing across multiple identical
-// instances of services and endpoints. When combined with a service discovery
-// system of record, it enables a more decentralized architecture, removing the
-// need for separate load balancers like HAProxy.
+// Package lb implements the client-side load balancer pattern. When combined
+// with a service discovery system of record, it enables a more decentralized
+// architecture, removing the need for separate load balancers like HAProxy.
 package lb
