d235befd41efdd623f634e0f1f618918ade8f58b
Chris Hines <github@cs-guy.com>
2016-07-19 21:40:43 -0400

    Update package docs and examples.
    - Add sections for basic usage, log contexts, dynamic context values,
      and concurrent safety.
    - Add executable tests that illustrate the new topics.
    - Update README.md to use log.NewSyncWriter in examples.

--- log/json_logger.go
+++ log/json_logger.go
@@ -13,7 +13,9 @@ type jsonLogger struct {
 }
 
 // NewJSONLogger returns a Logger that encodes keyvals to the Writer as a
-// single JSON object.
+// single JSON object. Each log event produces no more than one call to
+// w.Write. The passed Writer must be safe for concurrent use by multiple
+// goroutines if the returned Logger will be used concurrently.
 func NewJSONLogger(w io.Writer) Logger {
 	return &jsonLogger{w}
 }
