94628c7549780115a9990e965cf2661b01bef70a
Brian Kassouf <brian.m.kassouf@gmail.com>
2016-08-17 18:41:01 -0700

    Refactor away from passing a function to passing a struct with multiple options for signing keys

--- auth/jwt/middleware.go
+++ auth/jwt/middleware.go
@@ -2,8 +2,6 @@ package jwt
 
 import (
 	"errors"
-	"fmt"
-	"reflect"
 
 	"golang.org/x/net/context"
 
@@ -18,15 +16,35 @@ const (
 	JWTClaimsContextKey = "JWTClaims"
 )
 
+var (
+	ErrTokenContextMissing     = errors.New("Token up for parsing was not passed through the context")
+	ErrTokenInvalid            = errors.New("JWT Token was invalid")
+	ErrUnexpectedSigningMethod = errors.New("Unexptected signing method")
+	ErrKIDNotFound             = errors.New("Key ID was not found in key set")
+	ErrNoKIDHeader             = errors.New("Token doesn't have 'kid' header")
+)
+
+type Claims map[string]interface{}
+
+type KeySet map[string]struct {
+	Method jwt.SigningMethod
+	Key    []byte
+}
+
 // Create a new JWT token generating middleware, specifying signing method and the claims
 // you would like it to contain. Particularly useful for clients.
-func NewSigner(key string, method jwt.SigningMethod, claims jwt.Claims) endpoint.Middleware {
+func NewSigner(kid string, keys KeySet, claims Claims) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-			token := jwt.NewWithClaims(method, claims)
+			key, ok := keys[kid]
+			if !ok {
+				return nil, ErrKIDNotFound
+			}
 
+			token := jwt.NewWithClaims(key.Method, jwt.MapClaims(claims))
+			token.Header["kid"] = kid
 			// Sign and get the complete encoded token as a string using the secret
-			tokenString, err := token.SignedString([]byte(key))
+			tokenString, err := token.SignedString(key.Key)
 			if err != nil {
 				return nil, err
 			}
@@ -40,13 +58,13 @@ func NewSigner(key string, method jwt.SigningMethod, claims jwt.Claims) endpoint
 // Create a new JWT token parsing middleware, specifying a jwt.Keyfunc interface and the
 // signing method. Adds the resulting claims to endpoint context or returns error on invalid
 // token. Particularly useful for servers.
-func NewParser(keyFunc jwt.Keyfunc, method jwt.SigningMethod) endpoint.Middleware {
+func NewParser(keys KeySet) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 			// tokenString is stored in the context from the transport handlers
 			tokenString, ok := ctx.Value(JWTTokenContextKey).(string)
 			if !ok {
-				return nil, errors.New("Token up for parsing was not passed through the context")
+				return nil, ErrTokenContextMissing
 			}
 
 			// Parse takes the token string and a function for looking up the key. The latter is especially
@@ -54,22 +72,33 @@ func NewParser(keyFunc jwt.Keyfunc, method jwt.SigningMethod) endpoint.Middlewar
 			// head of the token to identify which key to use, but the parsed token (head and claims) is provided
 			// to the callback, providing flexibility.
 			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-				// Don't forget to validate the alg is what you expect:
-				if reflect.TypeOf(token.Method) != reflect.TypeOf(method) {
-					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				kid, ok := token.Header["kid"]
+				if !ok {
+					return nil, ErrNoKIDHeader
 				}
-				return keyFunc(token)
+
+				key, ok := keys[kid.(string)]
+				if !ok {
+					return nil, ErrKIDNotFound
+				}
+
+				// Don't forget to validate the alg is what you expect:
+				if token.Method != key.Method {
+					return nil, ErrUnexpectedSigningMethod
+				}
+
+				return key.Key, nil
 			})
 			if err != nil {
 				return nil, err
 			}
 
 			if !token.Valid {
-				return nil, errors.New("Could not parse JWT Token")
+				return nil, ErrTokenInvalid
 			}
 
 			if claims, ok := token.Claims.(jwt.MapClaims); ok {
-				ctx = context.WithValue(ctx, JWTClaimsContextKey, claims)
+				ctx = context.WithValue(ctx, JWTClaimsContextKey, Claims(claims))
 			}
 
 			return next(ctx, request)
