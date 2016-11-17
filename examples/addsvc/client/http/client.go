9d157bd86cc20f28de943d0b04451908f851ef3d
hsinih <hsinih.tu@gmail.com>
2016-11-17 02:37:16 -0800

    Correct typ-o in addsvc for concatEndpoint

--- examples/addsvc/client/http/client.go
+++ examples/addsvc/client/http/client.go
@@ -67,10 +67,10 @@ func New(instance string, tracer stdopentracing.Tracer, logger log.Logger) (adds
 		).Endpoint()
 		concatEndpoint = opentracing.TraceClient(tracer, "Concat")(concatEndpoint)
 		concatEndpoint = limiter(concatEndpoint)
-		sumEndpoint = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{
+		concatEndpoint = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{
 			Name:    "Concat",
 			Timeout: 30 * time.Second,
-		}))(sumEndpoint)
+		}))(concatEndpoint)
 	}
 
 	return addsvc.Endpoints{
