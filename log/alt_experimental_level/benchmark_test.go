99cde9832bb1ed010bf80f0185d7c554e07d9d3d
Steven E. Harris <seharris@us.ibm.com>
2017-02-10 17:13:23 -0500

    Benchmark various ratios of dropped log records

--- log/alt_experimental_level/benchmark_test.go
+++ log/alt_experimental_level/benchmark_test.go
@@ -1,6 +1,7 @@
 package level_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"testing"
 
@@ -9,48 +10,48 @@ import (
 )
 
 func BenchmarkNopBaseline(b *testing.B) {
-	benchmarkRunner(b, log.NewNopLogger())
+	singleRecordBenchmarkRunner(b, log.NewNopLogger())
 }
 
 func BenchmarkNopDisallowedLevel(b *testing.B) {
-	benchmarkRunner(b,
+	singleRecordBenchmarkRunner(b,
 		level.AllowingInfoAndAbove(log.NewNopLogger()))
 }
 
 func BenchmarkNopAllowedLevel(b *testing.B) {
-	benchmarkRunner(b,
+	singleRecordBenchmarkRunner(b,
 		level.AllowingAll(log.NewNopLogger()))
 }
 
 func BenchmarkJSONBaseline(b *testing.B) {
-	benchmarkRunner(b, log.NewJSONLogger(ioutil.Discard))
+	singleRecordBenchmarkRunner(b, log.NewJSONLogger(ioutil.Discard))
 }
 
 func BenchmarkJSONDisallowedLevel(b *testing.B) {
-	benchmarkRunner(b,
+	singleRecordBenchmarkRunner(b,
 		level.AllowingInfoAndAbove(log.NewJSONLogger(ioutil.Discard)))
 }
 
 func BenchmarkJSONAllowedLevel(b *testing.B) {
-	benchmarkRunner(b,
+	singleRecordBenchmarkRunner(b,
 		level.AllowingAll(log.NewJSONLogger(ioutil.Discard)))
 }
 
 func BenchmarkLogfmtBaseline(b *testing.B) {
-	benchmarkRunner(b, log.NewLogfmtLogger(ioutil.Discard))
+	singleRecordBenchmarkRunner(b, log.NewLogfmtLogger(ioutil.Discard))
 }
 
 func BenchmarkLogfmtDisallowedLevel(b *testing.B) {
-	benchmarkRunner(b,
+	singleRecordBenchmarkRunner(b,
 		level.AllowingInfoAndAbove(log.NewLogfmtLogger(ioutil.Discard)))
 }
 
 func BenchmarkLogfmtAllowedLevel(b *testing.B) {
-	benchmarkRunner(b,
+	singleRecordBenchmarkRunner(b,
 		level.AllowingAll(log.NewLogfmtLogger(ioutil.Discard)))
 }
 
-func benchmarkRunner(b *testing.B, logger log.Logger) {
+func singleRecordBenchmarkRunner(b *testing.B, logger log.Logger) {
 	b.ResetTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
@@ -58,18 +59,27 @@ func benchmarkRunner(b *testing.B, logger log.Logger) {
 	}
 }
 
-func BenchmarkManyDroppedRecords(b *testing.B) {
-	logger := level.AllowingInfoAndAbove(log.NewJSONLogger(ioutil.Discard))
-	b.ResetTimer()
-	b.ReportAllocs()
+func BenchmarkDroppedRecords(b *testing.B) {
+	logger := log.NewNopLogger()
+	logger = log.NewContext(logger).With("ts", log.DefaultTimestamp, "caller", log.DefaultCaller)
+	for _, dropped := range []uint{1, 3, 9, 99, 999} {
+		b.Run(fmt.Sprintf("%d-of-%d", dropped, dropped+1), func(b *testing.B) {
+			manyRecordBenchmarkRunner(b, logger, dropped)
+		})
+	}
+}
+
+func manyRecordBenchmarkRunner(b *testing.B, logger log.Logger, droppedRecords uint) {
+	logger = level.AllowingInfoAndAbove(logger)
 	debug := level.Debug(logger)
 	info := level.Info(logger)
+	b.ResetTimer()
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		debug.Log("foo", "1")
 		// Only this one will be retained.
-		info.Log("baz", "quux")
-		debug.Log("foo", "2")
-		debug.Log("foo", "3")
-		debug.Log("foo", "4")
+		info.Log("foo", "bar")
+		for ; droppedRecords != 0; droppedRecords-- {
+			debug.Log("baz", "quux")
+		}
 	}
 }
--- log/experimental_level/benchmark_test.go
+++ log/experimental_level/benchmark_test.go
@@ -1,6 +1,7 @@
 package level_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"testing"
 
@@ -9,48 +10,48 @@ import (
 )
 
 func BenchmarkNopBaseline(b *testing.B) {
-	benchmarkRunner(b, log.NewNopLogger())
+	singleRecordBenchmarkRunner(b, log.NewNopLogger())
 }
 
 func BenchmarkNopDisallowedLevel(b *testing.B) {
-	benchmarkRunner(b, level.New(log.NewNopLogger(),
+	singleRecordBenchmarkRunner(b, level.New(log.NewNopLogger(),
 		level.Allowed(level.AllowInfoAndAbove())))
 }
 
 func BenchmarkNopAllowedLevel(b *testing.B) {
-	benchmarkRunner(b, level.New(log.NewNopLogger(),
+	singleRecordBenchmarkRunner(b, level.New(log.NewNopLogger(),
 		level.Allowed(level.AllowAll())))
 }
 
 func BenchmarkJSONBaseline(b *testing.B) {
-	benchmarkRunner(b, log.NewJSONLogger(ioutil.Discard))
+	singleRecordBenchmarkRunner(b, log.NewJSONLogger(ioutil.Discard))
 }
 
 func BenchmarkJSONDisallowedLevel(b *testing.B) {
-	benchmarkRunner(b, level.New(log.NewJSONLogger(ioutil.Discard),
+	singleRecordBenchmarkRunner(b, level.New(log.NewJSONLogger(ioutil.Discard),
 		level.Allowed(level.AllowInfoAndAbove())))
 }
 
 func BenchmarkJSONAllowedLevel(b *testing.B) {
-	benchmarkRunner(b, level.New(log.NewJSONLogger(ioutil.Discard),
+	singleRecordBenchmarkRunner(b, level.New(log.NewJSONLogger(ioutil.Discard),
 		level.Allowed(level.AllowAll())))
 }
 
 func BenchmarkLogfmtBaseline(b *testing.B) {
-	benchmarkRunner(b, log.NewLogfmtLogger(ioutil.Discard))
+	singleRecordBenchmarkRunner(b, log.NewLogfmtLogger(ioutil.Discard))
 }
 
 func BenchmarkLogfmtDisallowedLevel(b *testing.B) {
-	benchmarkRunner(b, level.New(log.NewLogfmtLogger(ioutil.Discard),
+	singleRecordBenchmarkRunner(b, level.New(log.NewLogfmtLogger(ioutil.Discard),
 		level.Allowed(level.AllowInfoAndAbove())))
 }
 
 func BenchmarkLogfmtAllowedLevel(b *testing.B) {
-	benchmarkRunner(b, level.New(log.NewLogfmtLogger(ioutil.Discard),
+	singleRecordBenchmarkRunner(b, level.New(log.NewLogfmtLogger(ioutil.Discard),
 		level.Allowed(level.AllowAll())))
 }
 
-func benchmarkRunner(b *testing.B, logger log.Logger) {
+func singleRecordBenchmarkRunner(b *testing.B, logger log.Logger) {
 	b.ResetTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
@@ -58,20 +59,27 @@ func benchmarkRunner(b *testing.B, logger log.Logger) {
 	}
 }
 
-func BenchmarkManyDroppedRecords(b *testing.B) {
-	logger := level.New(log.NewJSONLogger(ioutil.Discard), level.Config{
-		Allowed: level.AllowInfoAndAbove(),
-	})
-	b.ResetTimer()
-	b.ReportAllocs()
+func BenchmarkDroppedRecords(b *testing.B) {
+	logger := log.NewNopLogger()
+	logger = log.NewContext(logger).With("ts", log.DefaultTimestamp, "caller", log.DefaultCaller)
+	for _, dropped := range []uint{1, 3, 9, 99, 999} {
+		b.Run(fmt.Sprintf("%d-of-%d", dropped, dropped+1), func(b *testing.B) {
+			manyRecordBenchmarkRunner(b, logger, dropped)
+		})
+	}
+}
+
+func manyRecordBenchmarkRunner(b *testing.B, logger log.Logger, droppedRecords uint) {
+	logger = level.New(logger, level.Allowed(level.AllowInfoAndAbove()))
 	debug := level.Debug(logger)
 	info := level.Info(logger)
+	b.ResetTimer()
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		debug.Log("foo", "1")
 		// Only this one will be retained.
-		info.Log("baz", "quux")
-		debug.Log("foo", "2")
-		debug.Log("foo", "3")
-		debug.Log("foo", "4")
+		info.Log("foo", "bar")
+		for ; droppedRecords != 0; droppedRecords-- {
+			debug.Log("baz", "quux")
+		}
 	}
 }
