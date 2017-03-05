01974e5478ff043c0d708f8c89526428161a754e
Bas van Beek <bas@xacte.com>
2017-03-05 10:06:46 +0100

    panic on codec error is better as it's a coding error

--- transport/grpc/_grpc_test/request_response.go
+++ transport/grpc/_grpc_test/request_response.go
@@ -2,39 +2,26 @@ package test
 
 import (
 	"context"
-	"errors"
 
 	pb "github.com/go-kit/kit/transport/grpc/_pb"
 )
 
 func encodeRequest(ctx context.Context, req interface{}) (interface{}, error) {
-	r, ok := req.(TestRequest)
-	if !ok {
-		return nil, errors.New("request encode error")
-	}
+	r := req.(TestRequest)
 	return &pb.TestRequest{A: r.A, B: r.B}, nil
 }
 
 func decodeRequest(ctx context.Context, req interface{}) (interface{}, error) {
-	r, ok := req.(*pb.TestRequest)
-	if !ok {
-		return nil, errors.New("request decode error")
-	}
+	r := req.(*pb.TestRequest)
 	return TestRequest{A: r.A, B: r.B}, nil
 }
 
 func encodeResponse(ctx context.Context, resp interface{}) (interface{}, error) {
-	r, ok := resp.(*TestResponse)
-	if !ok {
-		return nil, errors.New("response encode error")
-	}
+	r := resp.(*TestResponse)
 	return &pb.TestResponse{V: r.V}, nil
 }
 
 func decodeResponse(ctx context.Context, resp interface{}) (interface{}, error) {
-	r, ok := resp.(*pb.TestResponse)
-	if !ok {
-		return nil, errors.New("response decode error")
-	}
+	r := resp.(*pb.TestResponse)
 	return &TestResponse{V: r.V, Ctx: ctx}, nil
 }
