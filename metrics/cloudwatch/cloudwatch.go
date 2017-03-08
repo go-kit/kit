5616015c6949242c0adc5e2a27a48ebcd97f4c94
Cameron Stitt <cameron@cam.st>
2017-03-09 09:19:44 +1000

    Add comments and use default quantiles.

--- metrics/cloudwatch/cloudwatch.go
+++ metrics/cloudwatch/cloudwatch.go
@@ -5,8 +5,6 @@ import (
 
 	"time"
 
-	"strconv"
-
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -16,7 +14,11 @@ import (
 	"github.com/go-kit/kit/metrics/generic"
 )
 
-// CloudWatch ...
+// CloudWatch receives metrics observations and forwards them to CloudWatch.
+// Create a CloudWatch object, use it to create metrics, and pass those metrics as
+// dependencies to the components that will use them.
+//
+// To regularly report metrics to CloudWatch, use the WriteLoop helper method.
 type CloudWatch struct {
 	mtx        sync.RWMutex
 	namespace  string
@@ -27,7 +29,9 @@ type CloudWatch struct {
 	logger     log.Logger
 }
 
-// New ...
+// New returns a CloudWatch object that may be used to create metrics. Namespace is
+// applied to all created metrics and maps to the CloudWatch namespace.
+// Callers must ensure that regular calls to Send are performed, either manually or with one of the helper methods.
 func New(namespace string, logger log.Logger, svc *cloudwatch.CloudWatch) *CloudWatch {
 	return &CloudWatch{
 		namespace:  namespace,
@@ -39,6 +43,8 @@ func New(namespace string, logger log.Logger, svc *cloudwatch.CloudWatch) *Cloud
 	}
 }
 
+// NewCounter returns a counter. Observations are aggregated and emitted once
+// per write invocation.
 func (cw *CloudWatch) NewCounter(name string) *Counter {
 	c := NewCounter(name)
 	cw.mtx.Lock()
@@ -47,6 +53,8 @@ func (cw *CloudWatch) NewCounter(name string) *Counter {
 	return c
 }
 
+// NewGauge returns a gauge. Observations are aggregated and emitted once per
+// write invocation.
 func (cw *CloudWatch) NewGauge(name string) *Gauge {
 	g := NewGauge(name)
 	cw.mtx.Lock()
@@ -55,64 +63,80 @@ func (cw *CloudWatch) NewGauge(name string) *Gauge {
 	return g
 }
 
-func (cw *CloudWatch) NewHistogram(name string, quantiles []float64, buckets int) *Histogram {
-	h := NewHistogram(name, quantiles, buckets)
+// NewHistogram returns a histogram. Observations are aggregated and emitted as
+// per-quantile gauges, once per write invocation. 50 is a good default value
+// for buckets.
+func (cw *CloudWatch) NewHistogram(name string, buckets int) *Histogram {
+	h := NewHistogram(name, buckets)
 	cw.mtx.Lock()
 	cw.histograms[name] = h
 	cw.mtx.Unlock()
 	return h
 }
 
-// WriteLoop is a helper method that invokes WriteTo to the passed writer every
+// WriteLoop is a helper method that invokes Send every
 // time the passed channel fires. This method blocks until the channel is
 // closed, so clients probably want to run it in its own goroutine. For typical
 // usage, create a time.Ticker and pass its C channel to this method.
 func (cw *CloudWatch) WriteLoop(c <-chan time.Time) {
 	for range c {
-		cw.mtx.RLock()
-		defer cw.mtx.RUnlock()
-		now := time.Now()
-
-		datums := []*cloudwatch.MetricDatum{}
-
-		for name, c := range cw.counters {
-			datums = append(datums, &cloudwatch.MetricDatum{
-				MetricName: aws.String(name),
-				Dimensions: makeDimensions(c.c.LabelValues()...),
-				Value:      aws.Float64(c.c.Value()),
-				Timestamp:  aws.Time(now),
-			})
-		}
-
-		for name, g := range cw.gauges {
-			datums = append(datums, &cloudwatch.MetricDatum{
-				MetricName: aws.String(name),
-				Dimensions: makeDimensions(g.g.LabelValues()...),
-				Value:      aws.Float64(g.g.Value()),
-				Timestamp:  aws.Time(now),
-			})
-		}
-
-		for name, h := range cw.histograms {
-			for _, quantile := range h.quantiles {
-				quantileStr := strconv.FormatFloat(quantile, 'f', 2, 64)
-				datums = append(datums, &cloudwatch.MetricDatum{
-					MetricName: aws.String(fmt.Sprintf("%s_%s", name, quantileStr)),
-					Dimensions: makeDimensions(h.h.LabelValues()...),
-					Value:      aws.Float64(h.h.Quantile(quantile)),
-					Timestamp:  aws.Time(now),
-				})
-			}
-		}
-
-		_, err := cw.svc.PutMetricData(&cloudwatch.PutMetricDataInput{
-			Namespace:  aws.String(cw.namespace),
-			MetricData: datums,
-		})
-		if err != nil {
-			cw.logger.Log("during", "WriteLoop", "err", err)
+		if err := cw.Send(); err != nil {
+			cw.logger.Log("during", "Send", "err", err)
 		}
 	}
+}
+
+// Send will fire an api request to CloudWatch with the latest stats for
+// all metrics.
+func (cw *CloudWatch) Send() error {
+	cw.mtx.RLock()
+	defer cw.mtx.RUnlock()
+	now := time.Now()
+
+	datums := []*cloudwatch.MetricDatum{}
+
+	for name, c := range cw.counters {
+		datums = append(datums, &cloudwatch.MetricDatum{
+			MetricName: aws.String(name),
+			Dimensions: makeDimensions(c.c.LabelValues()...),
+			Value:      aws.Float64(c.c.Value()),
+			Timestamp:  aws.Time(now),
+		})
+	}
+
+	for name, g := range cw.gauges {
+		datums = append(datums, &cloudwatch.MetricDatum{
+			MetricName: aws.String(name),
+			Dimensions: makeDimensions(g.g.LabelValues()...),
+			Value:      aws.Float64(g.g.Value()),
+			Timestamp:  aws.Time(now),
+		})
+	}
+
+	for name, h := range cw.histograms {
+		for _, p := range []struct {
+			s string
+			f float64
+		}{
+			{"50", 0.50},
+			{"90", 0.90},
+			{"95", 0.95},
+			{"99", 0.99},
+		} {
+			datums = append(datums, &cloudwatch.MetricDatum{
+				MetricName: aws.String(fmt.Sprintf("%s_%s", name, p.s)),
+				Dimensions: makeDimensions(h.h.LabelValues()...),
+				Value:      aws.Float64(h.h.Quantile(p.f)),
+				Timestamp:  aws.Time(now),
+			})
+		}
+	}
+
+	_, err := cw.svc.PutMetricData(&cloudwatch.PutMetricDataInput{
+		Namespace:  aws.String(cw.namespace),
+		MetricData: datums,
+	})
+	return err
 }
 
 // Counter is a CloudWatch counter metric.
@@ -127,7 +151,7 @@ func NewCounter(name string) *Counter {
 	}
 }
 
-// With is a no-op.
+// With implements counter
 func (c *Counter) With(labelValues ...string) metrics.Counter {
 	return c.c.With(labelValues...)
 }
@@ -149,40 +173,43 @@ func NewGauge(name string) *Gauge {
 	}
 }
 
+// With implements gauge
 func (g *Gauge) With(labelValues ...string) metrics.Gauge {
 	return &Gauge{
 		g: g.g.With(labelValues...).(*generic.Gauge),
 	}
 }
 
+// Set implements gauge
 func (g *Gauge) Set(value float64) {
 	g.g.Set(value)
 }
 
+// Add implements gauge
 func (g *Gauge) Add(delta float64) {
 	g.g.Add(delta)
 }
 
 // Histogram is a CloudWatch histogram metric
 type Histogram struct {
-	quantiles []float64
-	h         *generic.Histogram
+	h *generic.Histogram
 }
 
 // NewHistogram returns a new usable histogram metric
-func NewHistogram(name string, quantiles []float64, buckets int) *Histogram {
+func NewHistogram(name string, buckets int) *Histogram {
 	return &Histogram{
-		quantiles: quantiles,
-		h:         generic.NewHistogram(name, buckets),
+		h: generic.NewHistogram(name, buckets),
 	}
 }
 
+// With implements histogram
 func (h *Histogram) With(labelValues ...string) metrics.Histogram {
 	return &Histogram{
 		h: h.h.With(labelValues...).(*generic.Histogram),
 	}
 }
 
+// Observe implements histogram
 func (h *Histogram) Observe(value float64) {
 	h.h.Observe(value)
 }
