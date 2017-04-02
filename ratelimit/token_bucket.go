881db165de5536ba4002062a362cb99b2b14c492
Gorka Lerchundi Osa <glertxundi@gmail.com>
2017-04-02 14:44:35 +0200

    [WIP] ratelimit: allow resolving token bucket using request scoped info

--- ratelimit/token_bucket.go
+++ ratelimit/token_bucket.go
@@ -18,8 +18,22 @@ var ErrLimited = errors.New("rate limit exceeded")
 // limiter based on a token-bucket algorithm. Requests that would exceed the
 // maximum request rate are simply rejected with an error.
 func NewTokenBucketLimiter(tb *ratelimit.Bucket) endpoint.Middleware {
+	return NewPerRequestTokenBucketLimiter(func(_ context.Context, _ interface{}) (*ratelimit.Bucket, error) {
+		return tb, nil
+	})
+}
+
+// NewPerRequestTokenBucketLimiter returns an endpoint.Middleware that acts as a
+// rate limiter based on a token-bucket algorithm retrieved in runtime by a
+// custom resolver defined by user. Requests that would exceed the maximum
+// request rate are simply rejected with an error.
+func NewPerRequestTokenBucketLimiter(resolver func(context.Context, interface{}) (*ratelimit.Bucket, error)) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (interface{}, error) {
+			tb, err := resolver(ctx, request)
+			if err != nil {
+				return nil, err
+			}
 			if tb.TakeAvailable(1) == 0 {
 				return nil, ErrLimited
 			}
