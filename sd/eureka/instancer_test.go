298086e19297e54d1457a4a33be453b86fabd1d4
Yuri Shkuro <ys@uber.com>
2017-05-16 23:50:42 -0400

    Fix tests

--- sd/eureka/instancer_test.go
+++ sd/eureka/instancer_test.go
@@ -26,8 +26,8 @@ func TestInstancer(t *testing.T) {
 
 	instancer := NewInstancer(connection, appNameTest, loggerTest)
 	defer instancer.Stop()
-	endpointer := sd.NewEndpointer(instancer, factory, loggerTest)
 
+	endpointer := sd.NewEndpointer(instancer, factory, loggerTest)
 	endpoints, err := endpointer.Endpoints()
 	if err != nil {
 		t.Fatal(err)
@@ -39,10 +39,6 @@ func TestInstancer(t *testing.T) {
 }
 
 func TestInstancerScheduleUpdates(t *testing.T) {
-	factory := func(string) (endpoint.Endpoint, io.Closer, error) {
-		return endpoint.Nop, nil, nil
-	}
-
 	connection := &testConnection{
 		instances:      []*fargo.Instance{instanceTest1},
 		application:    appUpdateTest,
@@ -51,51 +47,21 @@ func TestInstancerScheduleUpdates(t *testing.T) {
 
 	instancer := NewInstancer(connection, appNameTest, loggerTest)
 	defer instancer.Stop()
-	endpointer := sd.NewEndpointer(instancer, factory, loggerTest)
 
-	endpoints, _ := endpointer.Endpoints()
-	if want, have := 1, len(endpoints); want != have {
+	state := instancer.State()
+	if want, have := 1, len(state.Instances); want != have {
 		t.Errorf("want %d, have %d", want, have)
 	}
 
 	time.Sleep(50 * time.Millisecond)
 
-	endpoints, _ = endpointer.Endpoints()
-	if want, have := 2, len(endpoints); want != have {
+	state = instancer.State()
+	if want, have := 2, len(state.Instances); want != have {
 		t.Errorf("want %v, have %v", want, have)
 	}
 }
 
-func TestBadFactory(t *testing.T) {
-	factory := func(string) (endpoint.Endpoint, io.Closer, error) {
-		return nil, nil, errTest
-	}
-
-	connection := &testConnection{
-		instances:      []*fargo.Instance{instanceTest1},
-		application:    appUpdateTest,
-		errApplication: nil,
-	}
-
-	instancer := NewInstancer(connection, appNameTest, loggerTest)
-	defer instancer.Stop()
-	endpointer := sd.NewEndpointer(instancer, factory, loggerTest)
-
-	endpoints, err := endpointer.Endpoints()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if want, have := 0, len(endpoints); want != have {
-		t.Errorf("want %d, have %d", want, have)
-	}
-}
-
 func TestBadInstancerInstances(t *testing.T) {
-	factory := func(string) (endpoint.Endpoint, io.Closer, error) {
-		return endpoint.Nop, nil, nil
-	}
-
 	connection := &testConnection{
 		instances:      []*fargo.Instance{},
 		errInstances:   errTest,
@@ -105,23 +71,18 @@ func TestBadInstancerInstances(t *testing.T) {
 
 	instancer := NewInstancer(connection, appNameTest, loggerTest)
 	defer instancer.Stop()
-	endpointer := sd.NewEndpointer(instancer, factory, loggerTest)
 
-	endpoints, err := endpointer.Endpoints()
-	if err != nil {
-		t.Fatal(err)
+	state := instancer.State()
+	if state.Err == nil {
+		t.Fatal("expecting error")
 	}
 
-	if want, have := 0, len(endpoints); want != have {
+	if want, have := 0, len(state.Instances); want != have {
 		t.Errorf("want %d, have %d", want, have)
 	}
 }
 
 func TestBadInstancerScheduleUpdates(t *testing.T) {
-	factory := func(string) (endpoint.Endpoint, io.Closer, error) {
-		return endpoint.Nop, nil, nil
-	}
-
 	connection := &testConnection{
 		instances:      []*fargo.Instance{instanceTest1},
 		application:    appUpdateTest,
@@ -130,23 +91,23 @@ func TestBadInstancerScheduleUpdates(t *testing.T) {
 
 	instancer := NewInstancer(connection, appNameTest, loggerTest)
 	defer instancer.Stop()
-	endpointer := sd.NewEndpointer(instancer, factory, loggerTest)
 
-	endpoints, err := endpointer.Endpoints()
-	if err != nil {
-		t.Error(err)
+	state := instancer.State()
+	if state.Err != nil {
+		t.Error(state.Err)
 	}
-	if want, have := 1, len(endpoints); want != have {
+	if want, have := 1, len(state.Instances); want != have {
 		t.Errorf("want %d, have %d", want, have)
 	}
 
 	time.Sleep(50 * time.Millisecond)
 
-	endpoints, err = endpointer.Endpoints()
-	if err != nil {
-		t.Error(err)
+	state = instancer.State()
+	if state.Err == nil {
+		t.Fatal("expecting error")
 	}
-	if want, have := 1, len(endpoints); want != have {
+
+	if want, have := 0, len(state.Instances); want != have {
 		t.Errorf("want %v, have %v", want, have)
 	}
 }
