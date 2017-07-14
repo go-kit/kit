111107ec5237b6846e73833968c4b6caddad06e9
Ross McFarlane <ross@rossmcf.com>
2017-07-14 12:23:04 +0100

    Test ServerBefore / ServerAfter for JSON RPC.

--- transport/http/jsonrpc/server_test.go
+++ transport/http/jsonrpc/server_test.go
@@ -10,7 +10,9 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/transport/http/jsonrpc"
 )
 
@@ -89,6 +91,24 @@ func TestServerBadEncode(t *testing.T) {
 	expectErrorCode(t, jsonrpc.InternalError, buf)
 }
 
+func TestServerErrorEncoder(t *testing.T) {
+	//ecm := jsonrpc.EndpointCodecMap{
+	//"add": jsonrpc.EndpointCodec{
+	//Endpoint: func(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil },
+	//Decode:   func(context.Context, json.RawMessage) (interface{}, error) { return struct{}{}, nil },
+	//Encode:   func(context.Context, interface{}) (json.RawMessage, error) { return []byte{}, errors.New("oof") },
+	//},
+	//}
+	//handler := jsonrpc.NewServer(context.TODO(), ecm)
+	//server := httptest.NewServer(handler)
+	//defer server.Close()
+	//resp, _ := http.Post(server.URL, "application/json", addBody())
+	//if want, have := http.StatusOK, resp.StatusCode; want != have {
+	//t.Errorf("want %d, have %d", want, have)
+	//}
+	//buf, _ := ioutil.ReadAll(resp.Body)
+	//expectErrorCode(t, jsonrpc.InternalError, buf)
+}
 func TestServerUnregisteredMethod(t *testing.T) {
 	ecm := jsonrpc.EndpointCodecMap{}
 	handler := jsonrpc.NewServer(context.TODO(), ecm)
@@ -118,6 +138,80 @@ func TestServerHappyPath(t *testing.T) {
 	}
 	if r.Error != nil {
 		t.Fatalf("Unxpected error on response: %s", buf)
+	}
+}
+
+func TestMultipleServerBefore(t *testing.T) {
+	var done = make(chan struct{})
+	ecm := jsonrpc.EndpointCodecMap{
+		"add": jsonrpc.EndpointCodec{
+			Endpoint: endpoint.Nop,
+			Decode:   func(context.Context, json.RawMessage) (interface{}, error) { return struct{}{}, nil },
+			Encode:   func(context.Context, interface{}) (json.RawMessage, error) { return []byte{}, errors.New("oof") },
+		},
+	}
+	handler := jsonrpc.NewServer(
+		context.TODO(),
+		ecm,
+		jsonrpc.ServerBefore(func(ctx context.Context, r *http.Request) context.Context {
+			ctx = context.WithValue(ctx, "one", 1)
+
+			return ctx
+		}),
+		jsonrpc.ServerBefore(func(ctx context.Context, r *http.Request) context.Context {
+			if _, ok := ctx.Value("one").(int); !ok {
+				t.Error("Value was not set properly when multiple ServerBefores are used")
+			}
+
+			close(done)
+			return ctx
+		}),
+	)
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	http.Post(server.URL, "application/json", addBody()) // nolint
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for finalizer")
+	}
+}
+
+func TestMultipleServerAfter(t *testing.T) {
+	var done = make(chan struct{})
+	ecm := jsonrpc.EndpointCodecMap{
+		"add": jsonrpc.EndpointCodec{
+			Endpoint: endpoint.Nop,
+			Decode:   func(context.Context, json.RawMessage) (interface{}, error) { return struct{}{}, nil },
+			Encode:   func(context.Context, interface{}) (json.RawMessage, error) { return []byte{}, errors.New("oof") },
+		},
+	}
+	handler := jsonrpc.NewServer(
+		context.TODO(),
+		ecm,
+		jsonrpc.ServerAfter(func(ctx context.Context, w http.ResponseWriter) context.Context {
+			ctx = context.WithValue(ctx, "one", 1)
+
+			return ctx
+		}),
+		jsonrpc.ServerAfter(func(ctx context.Context, w http.ResponseWriter) context.Context {
+			if _, ok := ctx.Value("one").(int); !ok {
+				t.Error("Value was not set properly when multiple ServerAfters are used")
+			}
+
+			close(done)
+			return ctx
+		}),
+	)
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	http.Post(server.URL, "application/json", addBody()) // nolint
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for finalizer")
 	}
 }
 
