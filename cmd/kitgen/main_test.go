9b2f777fc5dfa37fe5d6402eb34b6de02da84c66
Judson <nyarly@gmail.com>
2017-07-17 11:33:08 -0700

    Broken sketch.

---
+++ cmd/kitgen/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestProcess(t *testing.T) {
+	cases, err := filepath.Glob("testdata/*/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testcase := func(dir string) {
+		name := filepath.Base(dir)
+		t.Run(name, func(t *testing.T) {
+			inpath := filepath.Join(dir, "in.go")
+			outpath := filepath.Join(dir, "out.go")
+
+			in, err := ioutil.ReadFile(inpath)
+			if err != nil {
+				t.Fatal(inpath, err)
+			}
+
+			expected, err := ioutil.ReadFile(outpath)
+			if err != nil {
+				t.Fatal(outpath, err)
+			}
+
+			actual, err := process(inpath, in)
+
+			if !bytes.Equal(expected, actual) {
+				errfile, err := ioutil.TempFile("", name)
+				if err != nil {
+					t.Fatal("opening tempfile for output", err)
+				}
+				io.Copy(errfile, actual)
+
+				t.Error("Processing output didn't match %q. Results recorded in %q.", outpath, errfile.Name())
+			}
+		})
+	}
+
+	for _, dir := range cases {
+		testcase(inpath, outpath)
+	}
+}
