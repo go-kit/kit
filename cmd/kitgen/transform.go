6246ebba2672bb52ce583cf2ff00377f1420ea68
Judson <nyarly@gmail.com>
2017-07-24 09:15:01 -0700

    Fruitful avenue. Committing for travel.

--- cmd/kitgen/transform.go
+++ cmd/kitgen/transform.go
@@ -2,14 +2,66 @@ package main
 
 import "go/ast"
 
-type transformer struct {
-	src *sourceContext
+func transformAST(ctx *sourceContext) (ast.Node, error) {
+	root := &ast.File{
+		Name:    ctx.pkg,
+		Imports: ctx.imports,
+		Decls:   []ast.Decl{},
+	}
+
+	for _, iface := range ctx.interfaces { //only one...
+		addStubStruct(root, iface)
+
+		for _, meth := range iface.methods {
+			addMethod(root, iface, meth)
+			addRequestStruct(root, meth)
+			addResponseStruct(root, meth)
+			addEndpointMaker(root, iface, meth)
+		}
+
+		addEndpointsStruct(root, iface)
+		addHTTPHandler(root, iface)
+
+		for _, meth := range iface.methods {
+			addDecoder(root, meth)
+			addEncoder(root, meth)
+		}
+	}
+	return root, nil
 }
 
-func (v *transformer) Visit(ast.Node) ast.Visitor {
-	return v
+func addStubStruct(root *ast.File, iface iface) {
+	root.Decls = append(root.Decls, iface.stubStructDecl())
 }
 
-func (v *transformer) err() error {
-	return nil
+func addMethod(root *ast.File, iface iface, meth method) {
+	root.Decls = append(root.Decls, meth.definition(iface))
+}
+
+func addRequestStruct(root *ast.File, meth method) {
+	root.Decls = append(root.Decls, meth.requestStruct())
+}
+
+func addResponseStruct(root *ast.File, meth method) {
+	root.Decls = append(root.Decls, meth.responseStruct())
+}
+
+func addEndpointMaker(root *ast.File, ifc iface, meth method) {
+	root.Decls = append(root.Decls, meth.endpointMaker(ifc))
+}
+
+func addEndpointsStruct(root *ast.File, ifc iface) {
+	root.Decls = append(root.Decls, ifc.endpointsStruct())
+}
+
+func addHTTPHandler(root *ast.File, ifc iface) {
+	root.Decls = append(root.Decls, ifc.httpHandler())
+}
+
+func addDecoder(root *ast.File, meth method) {
+	root.Decls = append(root.Decls, meth.decoderFunc())
+}
+
+func addEncoder(root *ast.File, meth method) {
+	root.Decls = append(root.Decls, meth.encoderFunc())
 }
