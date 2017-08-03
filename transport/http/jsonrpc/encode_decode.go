526b570e300634e0047fff20c00bdd06d544caf4
Ross McFarlane <ross@rossmcf.com>
2017-08-03 19:40:02 +0100

    Remove client-side JSON RPC funcs for now.

--- transport/http/jsonrpc/encode_decode.go
+++ transport/http/jsonrpc/encode_decode.go
@@ -12,20 +12,8 @@ import (
 // JSON decodes from the request body to the concrete response type.
 type DecodeRequestFunc func(context.Context, json.RawMessage) (request interface{}, err error)
 
-// EncodeRequestFunc encodes the passed request object into the HTTP request
-// object. It's designed to be used in HTTP clients, for client-side
-// endpoints. One straightforward EncodeRequestFunc could something that JSON
-// encodes the object directly to the request body.
-// type EncodeRequestFunc func(context.Context, *http.Request, interface{}) error
-
 // EncodeResponseFunc encodes the passed response object to the HTTP response
 // writer. It's designed to be used in HTTP servers, for server-side
 // endpoints. One straightforward EncodeResponseFunc could be something that
 // JSON encodes the object directly to the response body.
 type EncodeResponseFunc func(context.Context, interface{}) (response json.RawMessage, err error)
-
-// DecodeResponseFunc extracts a user-domain response object from an HTTP
-// response object. It's designed to be used in HTTP clients, for client-side
-// endpoints. One straightforward DecodeResponseFunc could be something that
-// JSON decodes from the response body to the concrete response type.
-// type DecodeResponseFunc func(context.Context, *http.Response) (response interface{}, err error)
