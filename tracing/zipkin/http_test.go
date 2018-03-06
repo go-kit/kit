cdd9bc5af77ed3efb03110f82de573a2f1d7eaa6
Bas van Beek <bas@tobin.nl>
2018-03-06 14:04:30 +0100

    add test for zipkin http server tracer

--- tracing/zipkin/http_test.go
+++ tracing/zipkin/http_test.go
@@ -11,8 +11,10 @@ import (
 
 	"github.com/openzipkin/zipkin-go"
 	"github.com/openzipkin/zipkin-go/model"
+	"github.com/openzipkin/zipkin-go/propagation/b3"
 	"github.com/openzipkin/zipkin-go/reporter/recorder"
 
+	"github.com/go-kit/kit/endpoint"
 	zipkinkit "github.com/go-kit/kit/tracing/zipkin"
 	kithttp "github.com/go-kit/kit/transport/http"
 )
@@ -51,21 +53,21 @@ func TestHttpClientTracePropagatesParentSpan(t *testing.T) {
 
 	_, err := ep(ctx, nil)
 	if err != nil {
-		t.Fatalf("unwanted error: %s", err.Error())
+		t.Fatalf("unexpected error: %s", err.Error())
 	}
 
 	spans := rec.Flush()
 	if want, have := 1, len(spans); want != have {
-		t.Fatalf("incorrect number of spans, wanted %d, got %d", want, have)
+		t.Fatalf("incorrect number of spans, want %d, have %d", want, have)
 	}
 
 	span := spans[0]
 	if span.SpanContext.ParentID == nil {
-		t.Fatalf("incorrect parent ID, got nil")
+		t.Fatalf("incorrect parent ID, want %s have nil", parentSpan.Context().ID)
 	}
 
 	if want, have := parentSpan.Context().ID, *span.SpanContext.ParentID; want != have {
-		t.Fatalf("incorrect parent ID, wanted %s, got %s", want, have)
+		t.Fatalf("incorrect parent ID, want %s, have %s", want, have)
 	}
 }
 
@@ -156,5 +158,60 @@ func testHTTPClientTraceCase(t *testing.T, responseStatusCode int, errTagValue s
 
 	if !reflect.DeepEqual(span.Tags, tags) {
 		t.Fatalf("invalid tags set, wanted %+v, got %+v", tags, span.Tags)
+	}
+}
+
+func TestHTTPServerTrace(t *testing.T) {
+	rec := recorder.NewReporter()
+	defer rec.Close()
+
+	// explicitely show we use the default of RPC shared spans in Zipkin as it
+	// is idiomatic for Zipkin to share span identifiers between client and
+	// server side.
+	tr, _ := zipkin.NewTracer(rec, zipkin.WithSharedSpans(true))
+
+	handler := kithttp.NewServer(
+		endpoint.Nop,
+		func(context.Context, *http.Request) (interface{}, error) { return nil, nil },
+		func(context.Context, http.ResponseWriter, interface{}) error { return nil },
+		zipkinkit.HTTPServerTrace(tr),
+	)
+
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	const httpMethod = "GET"
+
+	req, err := http.NewRequest(httpMethod, server.URL, nil)
+	if err != nil {
+		t.Fatalf("unable to create HTTP request: %s", err.Error())
+	}
+
+	parentSpan := tr.StartSpan("Dummy")
+
+	b3.InjectHTTP(req)(parentSpan.Context())
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to send HTTP request: %s", err.Error())
+	}
+	resp.Body.Close()
+
+	spans := rec.Flush()
+	if want, have := 1, len(spans); want != have {
+		t.Fatalf("incorrect number of spans, want %d, have %d", want, have)
+	}
+
+	if want, have := parentSpan.Context().TraceID, spans[0].SpanContext.TraceID; want != have {
+		t.Errorf("incorrect TraceID, want %+v, have %+v", want, have)
+	}
+
+	if want, have := parentSpan.Context().ID, spans[0].SpanContext.ID; want != have {
+		t.Errorf("incorrect span ID, want %d, have %d", want, have)
+	}
+
+	if want, have := httpMethod, spans[0].Name; want != have {
+		t.Errorf("incorrect span name, want %s, have %s", want, have)
 	}
 }
