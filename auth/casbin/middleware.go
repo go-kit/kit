bf51e5ee889a96124f7d81177a53b9bb5ce56d64
andreas <suekto.andreas@gmail.com>
2018-11-08 04:57:54 +0800

    add punctuation on casbin doc

--- auth/casbin/middleware.go
+++ auth/casbin/middleware.go
@@ -13,37 +13,37 @@ type contextKey string
 const (
 	// CasbinModelContextKey holds the key to store the access control model
 	// in context, it can be a path to configuration file or a casbin/model
-	// Model
+	// Model.
 	CasbinModelContextKey contextKey = "CasbinModel"
 
 	// CasbinPolicyContextKey holds the key to store the access control policy
 	// in context, it can be a path to policy file or an implementation of
-	// casbin/persist Adapter interface
+	// casbin/persist Adapter interface.
 	CasbinPolicyContextKey contextKey = "CasbinPolicy"
 
 	// CasbinEnforcerContextKey holds the key to retrieve the active casbin
-	// Enforcer
+	// Enforcer.
 	CasbinEnforcerContextKey contextKey = "CasbinEnforcer"
 )
 
 var (
 	// ErrModelContextMissing denotes a casbin model was not passed into
-	// the parsing of middleware's context
+	// the parsing of middleware's context.
 	ErrModelContextMissing = errors.New("CasbinModel is required in context")
 
 	// ErrPolicyContextMissing denotes a casbin policy was not passed into
-	// the parsing of middleware's context
+	// the parsing of middleware's context.
 	ErrPolicyContextMissing = errors.New("CasbinPolicy is required in context")
 
 	// ErrUnauthorized denotes the subject is not authorized to do the action
-	// intended on the given object, based on the context model and policy
+	// intended on the given object, based on the context model and policy.
 	ErrUnauthorized = errors.New("Unauthorized Access")
 )
 
 // NewEnforcer checks whether the subject is authorized to do the specified
-// action on the given object. If a valid access controlmodel and policy
+// action on the given object. If a valid access control model and policy
 // is given, then the generated casbin Enforcer is stored in the context
-// with CasbinEnforcer as the key
+// with CasbinEnforcer as the key.
 func NewEnforcer(
 	subject string, object interface{}, action string,
 ) endpoint.Middleware {
