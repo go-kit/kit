2409c7b071e61c4da6ad84ca8c3e703522247999
JP Robinson <john.paul.robinson@gmail.com>
2018-12-04 18:11:54 -0500

    [transport/http] adding request and response encoders for Protobuf

--- transport/http/server.go
+++ transport/http/server.go
@@ -3,10 +3,12 @@ package http
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
+	"github.com/golang/protobuf/proto"
 )
 
 // Server wraps an endpoint and implements http.Handler.
@@ -167,6 +169,45 @@ func EncodeJSONResponse(_ context.Context, w http.ResponseWriter, response inter
 		return nil
 	}
 	return json.NewEncoder(w).Encode(response)
+}
+
+// EncodeProtoResponse is an EncodeResponseFunc that serializes the response as Protobuf.
+// Many Proto-over-HTTP services can use it as a sensible default. If the response
+// implements Headerer, the provided headers will be applied to the response. If the
+// response implements StatusCoder, the provided StatusCode will be used instead of 200.
+func EncodeProtoResponse(ctx context.Context, w http.ResponseWriter, pres interface{}) error {
+	res, ok := pres.(proto.Message)
+	if !ok {
+		return errors.New("response does not implement proto.Message")
+	}
+	w.Header().Set("Content-Type", "application/x-protobuf")
+	if headerer, ok := w.(Headerer); ok {
+		for k := range headerer.Headers() {
+			w.Header().Set(k, headerer.Headers().Get(k))
+		}
+	}
+	code := http.StatusOK
+	if sc, ok := pres.(StatusCoder); ok {
+		code = sc.StatusCode()
+	}
+	w.WriteHeader(code)
+	if code == http.StatusNoContent {
+		return nil
+	}
+	if res == nil {
+		return nil
+	}
+	b, err := proto.Marshal(res)
+	if err != nil {
+		// returning an error here will cause a second header write, swallowing :(
+		return nil
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		// returning an error here will cause a second header write, swallowing :(
+		return nil
+	}
+	return nil
 }
 
 // DefaultErrorEncoder writes the error to the ResponseWriter, by default a
