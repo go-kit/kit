15ff8c232d32c98141e2042e30e4047118fee9cf
Peter Bourgon <peterbourgon@users.noreply.github.com>
2019-03-05 13:16:00 -0800

    log/level: improve examples (#845)
    * log/level: improve examples
    
    * log: mention package level in README
    
    * log/level: example line numbers changed

--- log/level/example_test.go
+++ log/level/example_test.go
@@ -9,17 +9,31 @@ import (
 )
 
 func Example_basic() {
-	// setup logger with level filter
+	logger := log.NewLogfmtLogger(os.Stdout)
+	level.Debug(logger).Log("msg", "this message is at thte debug level")
+	level.Info(logger).Log("msg", "this message is at the info level")
+	level.Warn(logger).Log("msg", "this message is at the warn level")
+	level.Error(logger).Log("msg", "this message is at the error level")
+
+	// Output:
+	// level=debug msg="this message is at thte debug level"
+	// level=info msg="this message is at the info level"
+	// level=warn msg="this message is at the warn level"
+	// level=error msg="this message is at the error level"
+}
+
+func Example_filtered() {
+	// Set up logger with level filter.
 	logger := log.NewLogfmtLogger(os.Stdout)
 	logger = level.NewFilter(logger, level.AllowInfo())
 	logger = log.With(logger, "caller", log.DefaultCaller)
 
-	// use level helpers to log at different levels
+	// Use level helpers to log at different levels.
 	level.Error(logger).Log("err", errors.New("bad data"))
 	level.Info(logger).Log("event", "data saved")
 	level.Debug(logger).Log("next item", 17) // filtered
 
 	// Output:
-	// level=error caller=example_test.go:18 err="bad data"
-	// level=info caller=example_test.go:19 event="data saved"
+	// level=error caller=example_test.go:32 err="bad data"
+	// level=info caller=example_test.go:33 event="data saved"
 }
