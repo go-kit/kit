ffac0f4777ed3554915b5002d8433eb71eec2e58
Peter Bourgon <peter@bourgon.org>
2019-03-05 10:08:53 -0800

    log/level: improve examples

--- log/level/example_test.go
+++ log/level/example_test.go
@@ -9,12 +9,26 @@ import (
 )
 
 func Example_basic() {
-	// setup logger with level filter
+	logger := log.NewLogfmtLogger(os.Stdout)
+	level.Debug(logger).Log("msg", "this message is at thte debug level")
+	level.Info(logger).Log("msg", "this message is at the info level")
+	level.Warn(logger).Log("msg", "this message is at the warn level")
+	level.Error(logger).Log("msg", "this message is at the error level")
+
+	// Output:
+	// level=debug msg="this message is at thte debug level"
+	// level=info msg="this message is at the info level"
+	// level=warn msg="this message is at the warn level"
+	// level=error msg="this message is at the error level"
+}
+
+func Example_filtered() {
+	// Set up logger with level filter.
 	logger := log.NewLogfmtLogger(os.Stdout)
 	logger = level.NewFilter(logger, level.AllowInfo())
 	logger = log.With(logger, "caller", log.DefaultCaller)
 
-	// use level helpers to log at different levels
+	// Use level helpers to log at different levels.
 	level.Error(logger).Log("err", errors.New("bad data"))
 	level.Info(logger).Log("event", "data saved")
 	level.Debug(logger).Log("next item", 17) // filtered
