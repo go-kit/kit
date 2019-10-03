0fadbe99ddb1ee87bcf5ea2ca9b84fcfe585c68b
Marat Reymers <16486128+maratori@users.noreply.github.com>
2019-10-03 21:34:22 +0300

    Introduce ErrorHandlerFunc (#915)

--- transport/error_handler.go
+++ transport/error_handler.go
@@ -26,3 +26,14 @@ func NewLogErrorHandler(logger log.Logger) *LogErrorHandler {
 func (h *LogErrorHandler) Handle(ctx context.Context, err error) {
 	h.logger.Log("err", err)
 }
+
+// The ErrorHandlerFunc type is an adapter to allow the use of
+// ordinary function as ErrorHandler. If f is a function
+// with the appropriate signature, ErrorHandlerFunc(f) is a
+// ErrorHandler that calls f.
+type ErrorHandlerFunc func(ctx context.Context, err error)
+
+// Handle calls f(ctx, err).
+func (f ErrorHandlerFunc) Handle(ctx context.Context, err error) {
+	f(ctx, err)
+}
