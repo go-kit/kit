eed6ce217b476e28bf5f0ce5fa6890e6c9f6cd4d
Chris Hines <github@cs-guy.com>
2020-01-05 23:14:52 -0500

    Add initial go.mod file
    The go.mod file was generated with the following sequence of commands:
    
    go mod init
    go mod tidy
    
    The first try resulted in two problems.
    
    - One problem was fixed by updating the import path of nats-server to
      use semantic import v2 in transport/nats/subscriber_test.go.
    - The other problem was fixed by changing the required version of
      go.etcd.io/etcd to v0.0.0-20191023171146-3cf2f69b5738 which
      corresponds to v3.4.3. We cannot use the v3.4.3 tag directly because
      of problems within the etcd repo itself described in
      https://github.com/etcd-io/etcd/issues/11154
    
    After those two changes a second go mod tidy succeeds and produces the
    go.mod file included in this commit.

--- transport/nats/subscriber_test.go
+++ transport/nats/subscriber_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/nats-io/nats-server/server"
+	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
 
 	"github.com/go-kit/kit/endpoint"
