439c4d2ed3ea0a07c80c8c8abe91f9e4d1f58a2c
Ludovic Fernandez <ldez@users.noreply.github.com>
2020-09-15 16:35:03 +0200

    metrics/generic: fix uint64 alignment (#1007)
    * fix: uint64 alignment in metrics/generic
    
    * test: adds a weak test on atomic alignment.

--- metrics/generic/generic.go
+++ metrics/generic/generic.go
@@ -18,9 +18,9 @@ import (
 
 // Counter is an in-memory implementation of a Counter.
 type Counter struct {
+	bits uint64 // bits has to be the first word in order to be 64-aligned on 32-bit
 	Name string
 	lvs  lv.LabelValues
-	bits uint64
 }
 
 // NewCounter returns a new, usable Counter.
@@ -81,9 +81,9 @@ func (c *Counter) LabelValues() []string {
 
 // Gauge is an in-memory implementation of a Gauge.
 type Gauge struct {
+	bits uint64 // bits has to be the first word in order to be 64-aligned on 32-bit
 	Name string
 	lvs  lv.LabelValues
-	bits uint64
 }
 
 // NewGauge returns a new, usable Gauge.
