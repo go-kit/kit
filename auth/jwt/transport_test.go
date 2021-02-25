ee40b538bf5ebb146ddfc951b4f07c9eb77b2cf8
shubhendra <withshubh@gmail.com>
2021-02-25 14:31:02 +0530

    Remove unnecessary use of `fmt.Sprintf`

--- auth/jwt/transport_test.go
+++ auth/jwt/transport_test.go
@@ -76,7 +76,7 @@ func TestGRPCToContext(t *testing.T) {
 	}
 
 	// Invalid Authorization header is passed
-	md["authorization"] = []string{fmt.Sprintf("%s", signedKey)}
+	md["authorization"] = []string{signedKey}
 	ctx = reqFunc(context.Background(), md)
 	token = ctx.Value(JWTTokenContextKey)
 	if token != nil {
