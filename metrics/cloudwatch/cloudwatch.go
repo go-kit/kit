f8b34921248215afaa993bdccd947a822b2a441d
Alexander Melentyev <alexander@melentyev.org>
2021-10-01 16:29:44 +0300

    Add apply method back

--- metrics/cloudwatch/cloudwatch.go
+++ metrics/cloudwatch/cloudwatch.go
@@ -48,6 +48,12 @@ type CloudWatch struct {
 
 type option func(*CloudWatch)
 
+func (s *CloudWatch) apply(opt option) {
+	if opt != nil {
+		opt(s)
+	}
+}
+
 func WithLogger(logger log.Logger) option {
 	return func(c *CloudWatch) {
 		c.logger = logger
